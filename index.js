b6f826a7fcb4711b7b86e0e38148c0fad91165b0
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-19 16:41:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 function switchOnCharmFromTip (tip) {
 switch (tip) {
   case 'generous':
-    "Thank you so much.";
+    return 'Thank you so much.';
   case 'not as generous':
-    'Thank you.';
-  case 'anything else':
-    'Bye.';
+    return 'Thank you.';
+  default:
+    return 'Bye.';
 }
 }
