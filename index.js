dc47e4c4fb896265613e6b92adde499e9ccfbf70
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-19 16:21:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let scuberGreetingForFeet;
+if (<= 400){
+  scuberGreetingForFeet = 'free'
+} else if (>=2000){
+  scuberGreetingForFeet = '$30'
+} else (>=2500){
+  scuberGreetingForFeet = 'not allowed'
+}
