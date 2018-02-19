a87b204a442914560f67c06f4faa5c297869afd2
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-19 16:37:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function scuberGreetingForFeet (feet) {
 function ternaryCheckCity (city) {
 return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
+
+
+function switchOnCharmFromTip (tip);
+
+switch (tip) {
+  case 'generous':
+    "Thank you so much"
+    break;
+  case 'not as generous':
+    'Thank you'
+    break;
+  case 'anything else':
+    'Bye'
+    break;
+}
